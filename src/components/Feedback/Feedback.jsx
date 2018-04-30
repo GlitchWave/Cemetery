f88be69b1890a1aa6502797b94e16abd1b42fd65
GlitchWave <v.fedyna@geniusee.com>
2018-04-30 17:35:11 +0300

    [+] skeleton of feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,7 +6,28 @@ class Feedback extends React.Component {
         super(props);
 
         this.state = {
+            textarea: ''
+        }
 
+        this.handleText = this.handleText.bind(this);
+    }
+
+    handleText(value) {
+        if((typeof value) === 'string') {
+            this.setState({textarea: value});
+        }
+    }
+
+    submitMessage() {
+        if(this.state.textarea.length !== 0) {
+            let message = {
+                text: this.state.textarea
+            };
+            /**
+             * Todo
+             * Send Message logic
+             */
+            this.setState({textarea: ''});
         }
     }
 
@@ -14,6 +35,9 @@ class Feedback extends React.Component {
         return (
             <div className="Feedback" >
                 <Header selected={'feedback'} />
+                <h2>Напишите нам письмо</h2>
+                <textarea cols="30" rows="10" value={this.state.textarea} onChange={(event) => this.handleText(event.target.value)} ></textarea>
+                <button  >Отправить</button>
             </div>
         )
     }
