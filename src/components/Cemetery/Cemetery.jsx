03f7b54e0ff0872d97da98c73ecedc591df930fa
GlitchWave <v.fedyna@geniusee.com>
2018-05-01 13:54:46 +0300

    [+] cemetry logic

--- src/components/Cemetery/Cemetery.jsx
+++ src/components/Cemetery/Cemetery.jsx
@@ -1,19 +1,73 @@
 import React from 'react';
 import Header from '../CommonDumbComponents/Header';
+import Interactor from './components/Interactor';
 
 class Cemetery extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            itemsToDisplay: [],
+            booked: [],
+            bookedPlaceClassName: 'Booked',
+            freePlace: 'FreePlace'
         }
+
+        this.bookPlace = this.bookPlace.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.createArray = this.createArray.bind(this);
+    }
+
+    bookPlace(placeId) {
+
+    }
+
+    onCancel(event) {
+        console.log(event.target.parentNode);
+    }
+
+    createArray(data) {
+        let places = [];
+
+        data.forEach(element => {
+            let key = Math.round(Math.random() * 10000);
+            if(this.state.booked.includes(+element.id)) {
+                places.push(
+                    <div data-id={element.id}
+                         key={key}
+                     >
+                        {element.description}
+                        <div onClick={this.onCancel} style={{display: 'none'}} >
+                        Cancel
+                        </div>
+                    </div>)
+            } else {
+                places.push(
+                    <div data-id={element.id}
+                         onClick={this.bookPlace}
+                         key={key}
+                     >
+                        {element.description}
+                        <div onClick={this.onCancel} >
+                        Cancel
+                        </div>
+                    </div>)
+            }
+        })
+
+        this.setState({itemsToDisplay: places});
+    }
+
+    componentWillMount() {
+        this.createArray([{id: 1, description: 'Loud place'}, {id: 2, description: 'Nice view'}]);
     }
 
     render() {
         return (
             <div className="Cemetery" >
                 <Header selected={'cemetery'} />
+                <Interactor itemsToDisplay={this.state.itemsToDisplay}
+                 />
             </div>
         )
     }
---
+++ src/components/Cemetery/components/Interactor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Interactor = (props) => {
+    return (
+        <div className="Interactor" >
+            {props.itemsToDisplay}
+        </div>
+    )
+}
+
+export default Interactor;
